6d44780bebc4fc28053c00ee099155757505bf0d
Kim Minki <wer153@naver.com>
2023-11-16 20:12:27 +0900

    feat: get_user_list api

--- account/apis.py
+++ account/apis.py
@@ -1,9 +1,23 @@
-from ninja import NinjaAPI
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from ninja import NinjaAPI, ModelSchema
+
+
+class UserSchema(ModelSchema):
+    class Config:
+        model = User
+        model_fields = ['id', 'username', 'first_name', 'last_name']
 
 
 api = NinjaAPI()
+User = get_user_model()
 
 
 @api.get("/healthcheck/")
 def healthcheck(_):
     return "healthy"
+
+
+@api.get("/users/", response=list[UserSchema])
+def get_users(request):
+    return list(User.objects.all())
