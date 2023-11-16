66e6e9d34b11798316581d53059c7d93b4d60090
Kim Minki <wer153@naver.com>
2023-11-16 20:27:26 +0900

    feat: create user api

--- account/apis.py
+++ account/apis.py
@@ -1,6 +1,9 @@
+from re import fullmatch
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from ninja import NinjaAPI, ModelSchema
+from ninja.errors import ValidationError
 
 
 class UserSchema(ModelSchema):
@@ -11,6 +14,7 @@ class UserSchema(ModelSchema):
 
 api = NinjaAPI()
 User = get_user_model()
+EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
 
 @api.get("/healthcheck/")
@@ -21,3 +25,10 @@ def healthcheck(_):
 @api.get("/users/", response=list[UserSchema])
 def get_user_list(request):
     return list(User.objects.all())
+
+
+@api.post("/users/", response=UserSchema)
+def create_user(request, email: str):
+    if not fullmatch(EMAIL_REGEX, email):
+        raise ValidationError("invalid email format")
+    return User.objects.create(username=email)
